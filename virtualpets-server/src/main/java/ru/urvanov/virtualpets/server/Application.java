a91c48068fde88ec0aaed06865de45f2f9a746be
Fedor Urvanov <fedor@urvanov.ru>
2024-01-08 16:36:24 +0300

    Started to configure Spring Boot application

--- virtualpets-server/src/main/java/ru/urvanov/virtualpets/server/Application.java
+++ virtualpets-server/src/main/java/ru/urvanov/virtualpets/server/Application.java
@@ -1,13 +1,13 @@
 package ru.urvanov.virtualpets.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        SpringApplication.run(Application.class, args);
     }
 
 }
