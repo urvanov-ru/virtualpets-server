aa48afd1eac1eec6f2d54f21230027446a02b26a
Fedor Urvanov <fedor@urvanov.ru>
2024-01-15 16:45:27 +0300

    Thymeleaf templates

--- virtualpets-server/src/main/java/ru/urvanov/virtualpets/server/config/WebConfig.java
+++ virtualpets-server/src/main/java/ru/urvanov/virtualpets/server/config/WebConfig.java
@@ -1,6 +1,16 @@
 package ru.urvanov.virtualpets.server.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("site/download").setViewName("download");
+        registry.addViewController("site/information").setViewName("information/list");
+        registry.addViewController("site/information/gameHelp").setViewName("information/gameHelp");
+    }
 }
